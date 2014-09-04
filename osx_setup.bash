85b780c1865252c4afa626cd13612c9234ebb539
Ben Keith <ben@progressivepublish.com>
2014-09-04 11:50:07 -0600

    Adds some more OSX defaults to be written

--- osx_setup.bash
+++ osx_setup.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
+defaults write com.apple.TextEdit CorrectSpellingAutomatically '0'
 defaults write com.apple.finder AppleShowAllFiles TRUE
+defaults write com.apple.finder EmptyTrashSecurely '1'
 killall Finder
 
 # To reverse this,
