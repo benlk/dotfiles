67dfece0d6cda5a8acf46f6301058b0043770f12
Ben Keith <ben@benlk.com>
2014-12-28 23:20:21 -0500

    permissions fixes, setup script fix, start on journal command

--- setup.bash
+++ setup.bash
@@ -3,19 +3,27 @@
 cp ~/.bashrc ~/.bashrc.bak
 cp ~/.dotfiles/.bashrc-redirect ~/.bashrc
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        # ...
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-	./osx_setup.bash
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-        # POSIX compatibility layer and Linux environment emulation for Windows
-elif [[ "$OSTYPE" == "msys" ]]; then
-        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-elif [[ "$OSTYPE" == "win32" ]]; then
-        # I'm not sure this can happen.
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-        # ...
-else
-        # Unknown.
-fi
+case "$OSTYPE" in
+'linux-gnu') 
+    # ...
+    ;;
+'darwin')
+    # Mac OSX
+    ./osx_setup.bash
+    ;;
+'cygwin')
+    # POSIX compatibility layer and Linux environment emulation for Windows
+    ;;
+'msys')
+    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+    ;;
+'win32')
+    # I'm not sure this can happen.
+    ;;
+'freebsd')
+    # ...
+    ;;
+*)
+    # Unknown.
+    ;;	
+esac
