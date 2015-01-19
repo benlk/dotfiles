4b8dbe39c6943e86aa03293a380a0d4f0f4880f5
Ben Keith <ben@benlk.com>
2015-01-19 10:42:05 -0500

    8192-bit keys in OSX

--- osx_setup.bash
+++ osx_setup.bash
@@ -5,6 +5,12 @@
 defaults write com.apple.TextEdit CorrectSpellingAutomatically '0'
 defaults write com.apple.finder AppleShowAllFiles TRUE
 defaults write com.apple.finder EmptyTrashSecurely '1'
+
+# http://blog.shiz.me/post/67305143330/8192-bit-rsa-keys-in-os-x
+# OSX doesn't by default like RSA keys longer than 4096 bits
+sudo defaults write /Library/Preferences/com.apple.crypto RSAMaxKeySize -int 8192
+sudo defaults write /Library/Preferences/com.apple.security RSAMaxKeySize -int 8192
+
 killall Finder
 
 # To reverse this,
