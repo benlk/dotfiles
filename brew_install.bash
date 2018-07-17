6f1d660bd7cf0a2370999d0b5f88ce9ff0eff972
Ben Keith <ben@benlk.com>
2018-07-16 22:25:56 -0400

    upgrade the brew install script with new curl and openssl and libssh2

--- brew_install.bash
+++ brew_install.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This assumes that you have already installed git and the xcode cli tools. How else did you git clone this repo?
-brew install bash-completion dos2unix mosh fortune the_silver_searcher imagemagick mysql
+brew install bash-completion dos2unix mosh fortune the_silver_searcher imagemagick mysql curl openssl libssh2
